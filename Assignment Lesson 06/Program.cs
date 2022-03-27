7bdc9c8913abc75b183e7a6c47854befd13e2d64
Hycient <onyeukwuhycient@gmail.com>
2022-03-27 23:44:17 +0100

    Added assignments 06 and 07

---
+++ Assignment Lesson 06/Program.cs
@@ -0,0 +1,77 @@
+﻿/* A program that reads several numbers from the console, and breaks when 
+  a number that is simultaneously divisible by 3 and 7 is encountered */
+static void Main()
+{
+  bool condition = true;
+  while(condition == true)
+  {
+    Console.WriteLine("Enter a number and hit the return key");
+    int input = int.Parse(Console.ReadLine());
+
+    if(input.GetType().ToString() != "System.Int32"){
+      Console.WriteLine("Input is not an integer");
+      condition = false;
+    }
+    else if(input % 3 == 0 && input % 7 == 0){
+      condition = false;
+    }
+  }
+}
+// Main();
+
+
+/* a program that prints to the console all the numbers divisible
+ by 7 from 1 to 300, excluding numbers that are both divisible by 
+ 5 and 7*/
+static void Indivisible()
+{
+  for(int i = 0; i <= 300; i++){
+    if(i % 7 == 0 && i % 5 != 0){
+      Console.WriteLine(i);
+    }
+  }
+}
+// Indivisible();
+
+
+/* Q10. Write a program that reads from the console a positive integer number
+  N (N < 20) and prints a matrix in the order in the provided diagram */
+static void Q10()
+{
+  int n = 4;
+  for(int i = 1; i <= n; i++){
+    for(int j = i; j <= (i + n - 1); j++){
+      Console.Write(j);
+    }
+    Console.WriteLine("");
+  }
+}
+// Q10();
+
+
+
+/* Q16.  Write a program that by a given integer N prints the numbers from 1 to N
+in random order.*/
+static void Q16(){
+  int[] arr = {1, 2, 3, 4, 5, 5, 6, 7, 8, 9, 10};
+  Random random = new System.Random();
+  int randFrom = random.Next(1, arr.Length);
+  int randTo = random.Next(1, arr.Length);
+
+  foreach (int i in arr)
+  {
+    int holder;
+    holder = arr[randFrom];
+    arr[randFrom] = arr[randTo];
+    arr[randTo] = holder;
+    Console.WriteLine(i);
+  }
+}
+// Q16();
+
+
+
+static void Q17(int a, int b){
+  int LCM = a * b;
+  // int GCD = 
+}
---
+++ Assignment Lesson 07/Program.cs
@@ -0,0 +1,177 @@
+﻿/* Q1. Write a program, which creates an array of 20 elements of type
+  integer and initializes each of the elements with a value equals to the
+  index of the element multiplied by 5. Print the elements to the console*/
+static void Q1()
+{
+  int[] arr = new int[20];
+
+  for(int i = 1; i < 20; i++){
+    arr[i] = (i * 5);
+    
+    // arr.CopyTo(i);
+    Console.Write($"{arr[i]}, ");
+  }
+}
+// Q1();
+
+
+
+/* Q2. Write a program, which reads two arrays from the console and checks
+  whether they are equal (two arrays are equal when they are of equal
+  length and all of their elements, which have the same index, are equal) */
+static void Q2()
+{
+  Console.WriteLine("Enter the value of the first array separated by commas");
+  string arr1 = Console.ReadLine();
+
+  Console.WriteLine("Enter the value of the second array separated by commas");
+  string arr2 = Console.ReadLine();
+
+  string[] splArr1 = arr1.Split(",");
+  string[] splArr2 = arr2.Split(",");
+
+  string message = "";
+  for(int i = 0; i < splArr1.Length; i++){
+    if(splArr1.Length == splArr2.Length && splArr1[i] == splArr2[i]){
+      message = "The both arrays entered are equal";
+    }
+    else{
+      message = "The both arrays entered are not equal";
+    }
+    
+  }
+  Console.WriteLine(message);
+}
+// Q2();
+
+
+/* Q3. Write a program, which compares two arrays of type char
+  lexicographically (character by character) and checks, which one is first
+  in the lexicographical order*/
+static void Q3()
+{
+  Console.WriteLine("Enter the character values of the first array separated by commas");
+  string arr1 = Console.ReadLine();
+
+  Console.WriteLine("Enter the character values of the second array separated by commas");
+  string arr2 = Console.ReadLine();
+
+  char[] splArr1 = new char[arr1.Length];
+  char[] splArr2 = new char[arr2.Length];
+
+  /* Determining which of the array has more content */
+  char[] longerArray;
+  if(splArr1.Length > splArr2.Length){
+    longerArray = splArr1;
+  }else{
+    longerArray = splArr2;
+  }
+
+  for(int i = 0; i < longerArray.Length; i++){
+    splArr1[i] = arr1[i];
+    splArr2[i] = arr2[i];
+  }
+
+  string message = "";
+  for(int i = 0; i < longerArray.Length; i++){
+    if((int)splArr1[i] < (int)splArr2[i] ){
+      message = "The first array comes first in the lexicographical order";
+    }else{
+      message = "The second array comes first in the lexicographical order";
+    }
+  }
+  Console.WriteLine(message);
+
+}
+// Q3();
+
+
+
+/* Q8. Write a program, which sorts an array using the
+algorithm " selection sort .*/
+static void Q8()
+{
+  int[] arr = {4,6,2,7,3,9,1,8,6,8,12};
+
+  for(int i = 0; i < arr.Length -1; i++){
+    for(int j = 0; j < arr.Length -1; j++){
+      if(arr[j] > arr[j + 1]){
+        int holder;
+        holder = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = holder;
+      }
+    }
+  }
+  /* Printing the result to console */
+  foreach(int item in arr){
+    Console.Write(item + ", ");
+  }
+  Console.WriteLine("");
+}
+// Q8();
+
+
+
+
+/* Q10. Write a program, which finds the most frequently occurring element in
+  an array */
+static void Q10()
+{
+  int[] numArr = {6, 4, 1, 1, 4, 2, 3, 4, 4, 1, 2, 4, 9, 3};
+  int[] holder = new int[numArr.Length];
+
+  for(int i = 0; i < numArr.Length; i++){
+    int tempVal = 0;
+    for(int j = 0; j < numArr.Length; j++){
+      if(numArr[i] == numArr[j]){
+        tempVal ++;
+        // numArr[i] = 0;
+      }
+    }
+    holder[i] = tempVal;
+  }
+
+  Console.WriteLine(Array.IndexOf(holder, holder.Max()));
+  Console.WriteLine($"The number {numArr[Array.IndexOf(holder, holder.Max())]} occours the most with a frequency of {holder.Max()} times!");
+}
+// Q10();
+
+static void Q13()
+{
+  Console.WriteLine("Numer the number of rows for the matrix");
+  int rows = int.Parse(Console.ReadLine());
+
+  Console.WriteLine("Numer the number of columns for the matrix");
+  int columns = int.Parse(Console.ReadLine());
+
+  for(int i = 0; i < (rows*rows); i+=rows){
+    Console.Write("|");
+    for(int j = 1; j <= columns; j++){
+      Console.Write($"  {j+i}, ");
+    }
+    Console.WriteLine("|");
+  }
+}
+// Q13();
+
+
+
+/* Q15. Write a program, which creates an array containing all Latin letters.
+  The user inputs a word from the console and as result the program
+  prints to the console the indices of the letters from the word */
+static void Q15()
+{
+  Console.WriteLine("Enter the word");
+  string input = Console.ReadLine();
+  char[] inputArr = new char[input.Length];
+  for(int i = 0; i < input.Length; i++){
+    inputArr[i] = input[i];
+  }
+
+  foreach(int item in inputArr){
+    Console.Write(item + " ");
+  }
+  Console.WriteLine("");
+}
+// Q15();
